50de241eedc40e62bcdb34024b423415966fc7f7
AxalotL <ladyginnic815@gmail.com>
2024-10-29 00:00:42 +0500

    fix villagers

--- src/main/java/com/axalotl/async/mixin/utils/SyncAllMixin.java
+++ src/main/java/com/axalotl/async/mixin/utils/SyncAllMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.ai.WardenAngerManager;
 import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.ai.pathing.PathMinHeap;
 import net.minecraft.util.math.random.CheckedRandom;
-import net.minecraft.village.VillagerGossips;
 import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.chunk.light.LevelPropagator;
 import net.minecraft.world.event.listener.SimpleGameEventDispatcher;
@@ -15,15 +14,14 @@ import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(value = {
     PathMinHeap.class, 
-    ChunkTickScheduler.class, 
+//    ChunkTickScheduler.class,
     LevelPropagator.class, 
     EntityNavigation.class, 
     CheckedRandom.class, 
     SimpleGameEventDispatcher.class, 
     AbstractCriterion.class, 
-    WardenAngerManager.class, 
-    WorldBorder.class, 
-    VillagerGossips.class, 
+    WardenAngerManager.class,
+    WorldBorder.class
 })
 public class SyncAllMixin {
 }
